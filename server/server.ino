ac7f78a37ab7c3ae480a69c68933c8f2e8c83dd3
Leoram <eleandroth@gmail.com>
2024-08-25 16:39:56 -0500

    codigo con wifi sensores y lecturas correspondientes guardadas en memoria, ademas se establecio una particion predeterminda

--- server/server.ino
+++ server/server.ino
@@ -1,99 +1,167 @@
-#include <Wire.h>
-#include <Adafruit_INA219.h>
+#include <WiFi.h>        //red wifi
+#include <WebServer.h>   // web comunicacion
+#include <ESPmDNS.h>     // no escribir ip 
+#include <Preferences.h> //guardad coniguracion
+#include "config.h"      // credenciales wifi
+//#include "union.h"      // partes de la pagina
+#include <Arduino.h>  // tareas paraleo
 
-#include <SPI.h>
-#include <SD.h>
+#include <Wire.h>             //leer corriente
+#include <Adafruit_INA219.h>  //leer corriente
 
-#include <DHT.h>
+#include <SPI.h>              //usar puerto SPI para SD
+#include <SD.h>               //usar SD
 
-#define CS_PIN 10  
+#include <DHT.h>              //sensor de temperatura
+
+#define CS_PIN 10           
 #define DHT_PIN 18
 #define ANG 1
+#define MXA 21
+#define MXB 47
+#define MXC 48
+#define LED 17
+#define B1 16
+#define B2 15
+#define B3 2
+#define B4 41
+#define B5 5
+#define B6 4
+#define OVP 6
+#define UVP 7
 
-
+const int pin[] = { MXA, MXB, MXC, LED, B1, B2, B3, B4, B5, B6, OVP, UVP };
 Adafruit_INA219 ina219(0x41);
 DHT dht(DHT_PIN, DHT11);
 
-void setup(void) 
-{
-  Serial.begin(115200);
-  while (!Serial) {
-      delay(1);
-  }
-    
-  pinMode(ANG_PIN, INPUT);
+WebServer server(80);
+Preferences p;
 
-  Serial.println("Hello!");
-  
-  if (! ina219.begin()) { 
-    Serial.println("Failed to find INA219 chip");
-    while (1) { delay(10); }
+
+const int nlec = 8;
+volatile float lec[nlec];
+
+
+/*         C -  B - A
+MB1 IO2    0 - 1 - 0
+MB2 IO1    0 - 0 - 1
+MB3 IO4    1 - 0 - 0
+MB4 IO6    1 - 1 - 0
+MB5 IO7    1 - 1 - 1
+MB6 IO5    1 - 0 - 1
+*/
+
+void canal(uint8_t valor) {
+  digitalWrite(MXC, valor & 0x01);
+  digitalWrite(MXB, (valor >> 1) & 0x01);
+  digitalWrite(MXA, (valor >> 2) & 0x01);
+}
+
+void leer() {
+  static const uint8_t canales[] = { 2, 1, 4, 6, 7, 5 };
+  for (uint8_t i = 0; i < 6; i++) {
+    canal(canales[i]);
+    lec[i] = analogRead(ANG);
+  }
+
+  lec[6] = ina219.getCurrent_mA();
+
+  float temp = dht.readTemperature();
+  if (!isnan(temp)) {
+    lec[7] = temp;
+  }
+}
+
+
+
+
+void wifi1(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void dns1(){
+  if (!MDNS.begin(name)) {  
+  Serial.println("Error setting up MDNS responder!");
+  while (1) {
+      delay(1000);
+  }
+  }
+}
+
+void initina() {
+  while (!ina219.begin()) {
+    Serial.println("Failed to find INA219 chip, retrying...");
+    delay(1000);
   }
   ina219.setCalibration_32V_2A();
 
   Serial.println("Measuring voltage and current with INA219 ...");
-  delay(2000);
+}
 
-  if (!SD.begin(CS_PIN)) {
-    Serial.println("No se pudo inicializar la tarjeta SD");
-    return;
+void initsd() {
+  while (!SD.begin(CS_PIN)) {
+    Serial.println("Failed to find SD, retrying...");
+    delay(1000);
   }
   Serial.println("Tarjeta SD inicializada correctamente");
+}
 
-  // Escribe en un archivo
-  File archivo = SD.open("/ejemplo.txt", FILE_WRITE);|
+void initpin() {
+  for (int i = 0; i < sizeof(pin) / sizeof(pin[0]); i++) {
+    pinMode(pin[i], OUTPUT);
+    digitalWrite(pin[i], 0);
+  }
+  pinMode(ANG, INPUT);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  Serial.println("Hello!");
+  wifi1();
+  dns1();
+  delay(1000);
+  initina();
+  delay(2000);
+  initsd();
+  initpin();
+
+}
+
+
+void loop(void) {
+
+  delay(1000);
+
+  leer();
+  for (int i = 0; i < nlec; i++) {
+    Serial.print(lec[i]);
+    Serial.print("  ");
+  }
+  File archivo = SD.open("/datos.txt", FILE_APPEND);
   if (archivo) {
-    archivo.println("Hola desde el ESP32-S3!");
+    for (int i = 0; i < nlec; i++) {
+      archivo.print(lec[i]);
+      archivo.print(",");
+    }
+    archivo.println();
     archivo.close();
     Serial.println("Se escribió en el archivo");
   } else {
     Serial.println("Error al abrir el archivo para escribir");
   }
-
-  // Lee del archivo
-  archivo = SD.open("/ejemplo.txt");
-  if (archivo) {
-    Serial.println("Contenido del archivo:");
-    while (archivo.available()) {
-      Serial.write(archivo.read());
-    }
-    archivo.close();
-  } else {
-    Serial.println("Error al abrir el archivo para leer");
-  }
-}
-
-void loop(void) 
-{
-  float shuntvoltage = 0;
-  float busvoltage = 0;
-  float current_mA = 0;
-  float loadvoltage = 0;
-  float power_mW = 0;
-
-  shuntvoltage = ina219.getShuntVoltage_mV();
-  busvoltage = ina219.getBusVoltage_V();
-  current_mA = ina219.getCurrent_mA();
-  power_mW = ina219.getPower_mW();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
-  
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
-  Serial.println("");
-
-  delay(2000);
-  Serial.println(dht.readTemperature());
-  Serial.println(" ---- ");
   delay(1000);
-
-  float lec = analogRead(ANG_PIN);
-  Serial.print(" analogo = ");
-  Serial.println(lec);
 }
-
-
-
-
